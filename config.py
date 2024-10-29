a2ca65fe7f2aaba7e36cdce41ad4b9add1370f53
abryukhanov <abryukhanov1994@mail.ru>
2024-10-29 13:27:19 +0700

    Final work

---
+++ config.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://www.kinopoisk.ru/"
+REGISTER_URL = "https://passport.yandex.ru/"
+USERNAME = "abryukhanov1994@yandex.ru"
+PASSWORD = "54937814As"
+API_BASE_URL = "https://api.kinopoisk.dev/v1.4"
+API_TOKEN = "JZY3CHS-4CQ4Q1S-GJYWHTK-A6T4A1D"
+HEADERS = {
+    "X-API-KEY": API_TOKEN
+}
